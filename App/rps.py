6887be050e6238043f46f19e91669838109dd584
Kyle Greenidge <kylegreen149@gmail.com>
2024-11-30 17:14:02 -0500

    Initialized start menu, created functions with no functionality

--- App/rps.py
+++ App/rps.py
@@ -36,3 +36,42 @@ def add_to_leaderboard(username, games, wins, losses, ties, longest_win_streak, 
     cursor = conn.cursor()
     cursor.execute(sql, [username, games, wins, losses, ties, longest_win_streak, longest_losing_streak, win_pct, loss_pct, ties_pct, rock_picks, paper_picks, scissors_picks])
     conn.commit()
+
+def home_screen():
+    while True:
+        prompt = """Rock Paper Scissors (Advanced Edition)
+        Play the classic Rock Paper Scissors game with advanced user stats!!
+
+        How to play:
+        When the prompt comes up, correctly type the words "rock", "paper", or "scissors"
+        That's it!!
+
+        Select an option:
+        1. Sign up as a new player
+        2. Continue progress as an existing player
+        3. See Leaderboard
+        4. Quit
+
+
+        >>"""
+        option = input(prompt)
+        if option == "1":
+            signUp()
+        elif option == "2":
+            signIn()
+        elif option == "3":
+            show_leaderboard()
+        elif option == "4": 
+            print("Closing game... Come back soon!")
+            exit()
+        else:
+            print("Enter a number from 1-3 only.")
+
+def signUp():
+    pass
+
+def signIn():
+    pass
+
+def show_leaderboard():
+    pass
