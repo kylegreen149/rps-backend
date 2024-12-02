b6d1e69dd7cd11bf46776ab4b065ef6b158a9179
Kyle Greenidge <kylegreen149@gmail.com>
2024-12-01 20:16:38 -0500

    Added second param to play_rps function, developed sign in and gt_user_stats function

--- App/rps.py
+++ App/rps.py
@@ -40,20 +40,20 @@ def add_to_leaderboard(username, games, wins, losses, ties, longest_win_streak, 
 def home_screen():
     while True:
         prompt = """Rock Paper Scissors (Advanced Edition)
-        Play the classic Rock Paper Scissors game with advanced user stats!!
+Play the classic Rock Paper Scissors game with advanced user stats!!
 
-        How to play:
-        When the prompt comes up, correctly type the words "rock", "paper", or "scissors"
-        That's it!!
+How to play:
+When the prompt comes up, correctly type the words "rock", "paper", or "scissors"
+That's it!!
 
-        Select an option:
-        1. Sign up as a new player
-        2. Continue progress as an existing player
-        3. See Leaderboard
-        4. Quit
+Select an option:
+1. Sign up as a new player
+2. Continue progress as an existing player
+3. See Leaderboard
+4. Quit
 
 
-        >>"""
+>> """
         option = input(prompt)
         if option == "1":
             signUp()
@@ -65,17 +65,66 @@ def home_screen():
             print("Closing game... Come back soon!")
             exit()
         else:
-            print("Enter a number from 1-3 only.")
+            print("Enter a number from 1-4 only.")
 
 def signUp():
     enter_username = input("Enter a username to play with: ")
-    play_rps(enter_username)
+    if enter_username != "":
+        stats = {
+            "games": 0,
+            "wins": 0,
+            "losses": 0,
+            "ties": 0,
+            "longest_win_streak": 0,
+            "longest_losing_streak": 0,
+            "win_pct": 0.0,
+            "loss_pct": 0.0,
+            "ties_pct": 0.0,
+            "rock_picks": 0,
+            "paper_picks": 0,
+            "scissors_picks": 0
+        }
+        play_rps(enter_username, stats)
+    else:
+        print("Please enter a valid username!")
+        enter_username = input("Enter a username to play with: ")
+
+
+def get_user_stats(username):
+    # Connect to the database
+    conn = sqlite3.connect('leaderboard.db')
+    cursor = conn.cursor()
+
+    # Execute the SQL query to find the user
+    cursor.execute("SELECT * FROM leaderboard WHERE username = ?", (username,))
+    user_stats = cursor.fetchone()
+
+    # Close the database connection
+    conn.close()
+
+    # Check if user was found and return stats
+    if user_stats:
+        return user_stats
+    else:
+        print("Username not found.")
+        return None
 
 def signIn():
-    pass
+    enter_username = input("Enter an existing username to continue playing: ")
+    user_stats = get_user_stats(enter_username)
+    if user_stats:
+        play_rps(enter_username, user_stats)
+    else:
+        print("Make sure you are entering a username that already exist!")
+        enter_username = input("Enter an existing username to continue playing: ")
+
 
 def show_leaderboard():
     pass
 
-def play_rps(username):
-    pass
+def play_rps(username, stats):
+    print("Welcome back,", username, "!")
+    print("Here are your stats:")
+    print(stats)
+
+home_screen()
